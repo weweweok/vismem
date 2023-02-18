8aa4831f78ff6b84de3652f6a4545c0acebf47a9
Kai <100256521+weweweok@users.noreply.github.com>
2023-02-18 12:49:46 +0900

    Delete src directory

--- src/graph_memo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GraphMemoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'graph_memo'
--- src/graph_memo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = "graph_memo"
-urlpatterns = [
-    path('', views.define_graph, name='define_graph'),
-]
