97ce8252d8ecab86f8882bd92537778be0433a35
kaito miyoshi <combination.kyoukyou@gmail.com>
2023-03-05 12:00:07 +0900

    add  create recode

--- src/graph_memo/models.py
+++ src/graph_memo/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Graph_nodes(models.Model):
-    nodes_id = models.CharField(max_length=100)
-    nodes_text = models.CharField(max_length=30)
+    node_id = models.CharField(max_length=100)
+    node_text = models.CharField(max_length=30)
 
 class Graph_edges(models.Model):
-    edges_from = models.CharField(max_length=200)
-    edges_to = models.CharField(max_length=200)
+    edge_from = models.CharField(max_length=200)
+    edge_to = models.CharField(max_length=200)
--- src/graph_memo/views.py
+++ src/graph_memo/views.py
@@ -12,17 +12,28 @@ import pprint
 def define_graph(request):
     #試作中、理想は各リストにデータベースのデータが格納されていること
     data = {
-            'nodes':[],
+            'nodes':[{"id": data.node_id ,"label": data.node_text} for data in Graph_nodes.objects.all()],
             'edges':[],
             }
     if request.method == 'POST':
-            print("success")
-            posted_data = json.loads(request.body)
-            print("node data: {}".format(posted_data["nodes"]))
-            print()
-            print("edges data: {}".format(posted_data["edges"]))
-            return render(request,"graph_memo/graph_memo.html", {'data_json': json.dumps(posted_data)})
-            
+        print("success")
+        posted_data = json.loads(request.body)
+        print("node data: {}".format(posted_data["nodes"]))
+        print()
+        print("edges data: {}".format(posted_data["edges"]))
+
+        Graph_nodes.objects.all().delete()
+        Graph_edges.objects.all().delete()
+
+        for data in posted_data["nodes"]:
+                Graph_nodes.objects.create(node_id=data["id"], node_text=data["label"])
+        print(Graph_nodes.objects.distinct().values_list("id"))
+
+        for data in posted_data["edges"]:
+                Graph_edges.objects.create(edge_from=data["from"], edge_to=data["to"])
+
+        return render(request,"graph_memo/graph_memo.html", {'data_json': json.dumps(posted_data)})
+
     return render(request,"graph_memo/graph_memo.html", {'data_json': json.dumps(data)})
 
 
